bbac2ebe026e2ca24d6aaef7aecf52bd55b671dc
Nisitha529 <silvanisithamaneesha@gmail.com>
2026-03-09 23:49:31 +0530

    Update control_unit.vhd

--- rtl/sources/control_unit.vhd
+++ rtl/sources/control_unit.vhd
@@ -59,11 +59,123 @@ begin
             ALU_OP_TYPE_SUB when (opcode = INSTR_OP_REG_REG and funct3 = INSTR_F3_SUB and funct7 = INSTR_F7_SUB) or
 
             -- Subtraction for branching (BEQ, BNE)
-            (opcode = INSTR_OP_BRANCH and (funct3 = INSTR_F3_BEQ or funct3 = INSTR_F3_BNE)) else
+                                 (opcode = INSTR_OP_BRANCH and (funct3 = INSTR_F3_BEQ or funct3 = INSTR_F3_BNE)) else
 
 
-            -- Set less than (signed): Used for SLTI, SLTand for signed branches
-            ALU_OP_TYPE_SLT when 
+            -- Set less than (signed): Used for SLTI, SLT and for signed branches
+            ALU_OP_TYPE_SLT when (opcode = INSTR_OP_REG_IMM and funct3 = INSTR_F3_SLTI) or 
+
+            -- 
+                                 (opcode = INSTR_OP_REG_REG and funct3 = INSTR_F3_SLT and funct7 = INSTR_F7_SLT) or 
+                                 
+                                 
+                                 (opcode = INSTR_OP_BRANCH and (funct3 = INSTR_F3_BLT or funct3 = INSTR_F3_BGE)) else
+
+            -- Set less than (unsigned): used for SLTIU, SLTU, and for unsigned branches
+            ALU_OP_TYPE_SLTU when (opcode = INSTR_OP_REG_IMM and funct3 = INSTR_F3_SLTIU) or 
+                  
+                                  (opcode = INSTR_OP_REG_REG and funct3 = INSTR_F3_SLTU and funct7 = INSTR_F7_SLTU) or 
+                                  
+                                  (opcode = INSTR_OP_BRANCH and (funct3 = INSTR_F3_BLTU or funct3 = INSTR_F3_BGEU)) else
+
+            -- Bitwise AND
+            ALU_OP_TYPE_AND when (opcode = INSTR_OP_REG_IMM and funct3 = INSTR_F3_ANDI) or 
+                   
+                                 (opcode = INSTR_OP_REG_REG and funct3 = INSTR_F3_AND and funct7 = INSTR_F7_AND) else
+
+            -- Bitwise OR
+            ALU_OP_TYPE_OR  when (opcode = INSTR_OP_REG_IMM and funct3 = INSTR_F3_ORI) or 
+              
+                                 (opcode = INSTR_OP_REG_REG and funct3 = INSTR_F3_OR and funct7 = INSTR_F7_OR) else
+
+            -- Bitwise XOR
+            ALU_OP_TYPE_XOR when (opcode = INSTR_OP_REG_IMM and funct3 = INSTR_F3_XORI) or 
+            
+                                 (opcode = INSTR_OP_REG_REG and funct3 = INSTR_F3_XOR and funct7 = INSTR_F7_XOR) else
+
+            -- Logical left shift
+            ALU_OP_TYPE_SLL when (opcode = INSTR_OP_REG_IMM and funct3 = INSTR_F3_SLLI and funct7 = INSTR_F7_SLLI) or 
+            
+                                 (opcode = INSTR_OP_REG_REG and funct3 = INSTR_F3_SLL and funct7 = INSTR_F7_SLL) else
+
+            -- Logical right shift
+            ALU_OP_TYPE_SRL when (opcode = INSTR_OP_REG_IMM and funct3 = INSTR_F3_SRLI and funct7 = INSTR_F7_SRLI) or 
+            
+                                 (opcode = INSTR_OP_REG_REG and funct3 = INSTR_F3_SRL and funct7 = INSTR_F7_SRL) else
+
+            -- Arithmetic right shift
+            ALU_OP_TYPE_SRA when (opcode = INSTR_OP_REG_IMM and funct3 = INSTR_F3_SRAI and funct7 = INSTR_F7_SRAI) or 
+            
+                                 (opcode = INSTR_OP_REG_REG and funct3 = INSTR_F3_SRA and funct7 = INSTR_F7_SRA) else
+
+            -- Default : ADD 
+            ALU_OP_TYPE_ADD;
+
+  -- ALU operand B selection
+  alu_use_imm <= '1' when opcode = INSTR_OP_REG_IMM or 
+                     
+                          opcode = INSTR_OP_JALR or 
+                          
+                          opcode = INSTR_OP_LOAD or 
+                          
+                          opcode = INSTR_OP_STORE else
+                   
+                 '0';  
+            
+  -- Register file write enable
+  write_rd    <= '0' when opcode = INSTR_OP_BRANCH or 
+  
+                          opcode = INSTR_OP_STORE or 
+                          
+                          opcode = INSTR_OP_FENCE or 
+                          
+                          opcode = INSTR_OP_SYSTEM else
+                 '1';
+
+  -- Data memory write enable
+  write_mem   <= '1' when opcode = INSTR_OP_STORE else 
+  
+                 '0';
+
+  -- Memory access enable
+  mem_access  <= '1' when opcode = INSTR_OP_LOAD or 
+  
+                          opcode = INSTR_OP_STORE else 
+                          
+                 '0';
+
+  -- Branch taken condition
+  take_branch <= '1' when (branch = BRANCH_TYPE_BEQ and alu_result = x"00000000") or 
+  
+                          (branch = BRANCH_TYPE_BNE and alu_result /= x"00000000") or 
+                          
+                          (branch = BRANCH_TYPE_BLT and alu_result = x"00000001") or 
+                          
+                          (branch = BRANCH_TYPE_BGE and alu_result = x"00000000") else
+                   
+                 '0';       
+
+  -- Register write data source selection
+  rd_data_src <= RD_DATA_SRC_PC_IMM       when opcode = INSTR_OP_AUIPC else
+                   
+                 RD_DATA_SRC_PC_4         when opcode = INSTR_OP_JAL or opcode = INSTR_OP_JALR else
+                   
+                 RD_DATA_SRC_IMM          when opcode = INSTR_OP_LUI else
+
+                 RD_DATA_SRC_MEM_DATA_OUT when opcode = INSTR_OP_LOAD else
+
+                 RD_DATA_SRC_ALU_RESULT;   
+
+  -- Memory access width
+  mem_access_width <= MEM_ACCESS_WIDTH_32 when (opcode = INSTR_OP_LOAD  and funct3 = INSTR_F3_LW) or 
+                                               
+                                               (opcode = INSTR_OP_STORE and funct3 = INSTR_F3_SW) else
+        
+                      MEM_ACCESS_WIDTH_16 when (opcode = INSTR_OP_LOAD  and (funct3 = INSTR_F3_LH or funct3 = INSTR_F3_LHU)) or 
+                      
+                                               (opcode = INSTR_OP_STORE and funct3 = INSTR_F3_SH) else
+                      
+                      MEM_ACCESS_WIDTH_8;
 
 end architecture;
 
